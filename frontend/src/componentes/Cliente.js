df7e531bad005d3be44cae7e3f0cf930755aa411
Alvaro Fuentes <alvaro.fuentesp@sonda.com>
2018-11-12 12:51:52 -0300

    Repara dependencias

---
+++ frontend/src/componentes/Cliente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Cliente extends React.Component {
+
+    render () {
+        return (
+            <p>Componente cliente</p>
+        )
+    }
+}
+
+export default Cliente;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Cliente from './componentes/Cliente';
+import NotFound from './componentes/NotFound';
+
+const Root = () => {
+    return (
+        <Router basename="/sonda">
+            <Switch>
+                <Route exact path="/" component={Cliente} />
+                <Route exact path="/cliente" component={Cliente} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
+    )
+}
+
+render(<Root/>, document.querySelector('#main'));
+
+
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 //serviceWorker.unregister();
